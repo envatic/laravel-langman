d1dde0febccfc00910a7ca4829c317eca52761e4
ofumbi <ofuzak@gmail.com>
2019-11-04 07:09:33 +0300

    Add support for vue-i18n language Transaltions
    Grab translations from  vue-i18n  translations in the components
    
    Signed-off-by: ofumbi <ofuzak@gmail.com>

--- src/LangmanServiceProvider.php
+++ src/LangmanServiceProvider.php
@@ -25,7 +25,7 @@ class LangmanServiceProvider extends ServiceProvider
             return new Manager(
                 new Filesystem,
                 $this->app['config']['langman.path'],
-                array_merge($this->app['config']['view.paths'], [$this->app['path']])
+                array_merge($this->app['config']['view.paths'],$this->app['config']['langman.vuepath'], [$this->app['path']])
             );
         });
 
